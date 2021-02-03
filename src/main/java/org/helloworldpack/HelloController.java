42f5df0380572fe2b132171709c780f9c7e7c43d
Darin Debrestian <dad19a@my.fsu.edu>
2021-02-03 13:19:57 -0500

    Initial Commit of Hello World Spring Boot App

---
+++ src/main/java/org/helloworldpack/HelloController.java
@@ -0,0 +1,11 @@
+package org.helloworldpack;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public static String rootpage() {
+        return "Hello World!";
+    }
+}
